2baf50f5b5d40661b2eabc1d0a6feccc572b2a88
Matej Mamic <matejmamic10@gmail.com>
2024-04-24 20:30:12 +0200

    zadatak

---
+++ src/components/funkcija.jsx
@@ -0,0 +1,3 @@
+export default function Text() {
+  return <p>funkcija text</p>;
+}
---
+++ src/components/objekt.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default class Objekt extends React.Component {
+  render() {
+    return <p>Text objekt</p>;
+  }
+}
